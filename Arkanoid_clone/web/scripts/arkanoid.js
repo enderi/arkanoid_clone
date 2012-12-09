e98eb0a5ced69e610de3e1fd2969bdad01585f31
Janne Vento <jannejvento@gmail.com>
2012-12-09 22:36:06 +0200

    Initial commit
    framework files and stuff

---
+++ Arkanoid_clone/web/scripts/arkanoid.js
@@ -0,0 +1,18 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+$(document).ready(function(){
+    var context = $("#gamearea")[0].getContext("2d");
+    context.clearRect(0,0,640,480);
+    context.fillStyle = "rgb(60,60,60)";
+    context.fillRect(0,0,640,480);
+    context.clearRect(21,21,598,450);
+    context.fillStyle = "rgb(0,230,0)";
+    for(var i=21;i<598;i=i+46){
+        context.fillStyle = "rgb(0,0,0)";
+        context.fillRect(i,100,46,25);
+        context.fillStyle = "rgb(0,230,0)";
+        context.fillRect(i+1,101,44,23);
+    }
+});
